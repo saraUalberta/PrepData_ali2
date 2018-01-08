8c0d2bcef5f5908d1e19246e29af430d90934fae
hejazizo <hejazizo@ualberta.ca>
2018-01-08 19:14:26 +0000

    thirdRun

---
+++ 3220/scripts/recon-all.cmd
@@ -0,0 +1,80 @@
+
+
+#---------------------------------
+# New invocation of recon-all Mon Jan  1 19:38:56 UTC 2018 
+
+ mri_convert /home/ubuntu/sMRI/3220.nii /usr/local/freesurfer/subjects/3220/mri/orig/001.mgz 
+
+#--------------------------------------------
+#@# MotionCor Mon Jan  1 19:39:10 UTC 2018
+
+ cp /usr/local/freesurfer/subjects/3220/mri/orig/001.mgz /usr/local/freesurfer/subjects/3220/mri/rawavg.mgz 
+
+
+ mri_convert /usr/local/freesurfer/subjects/3220/mri/rawavg.mgz /usr/local/freesurfer/subjects/3220/mri/orig.mgz --conform 
+
+
+ mri_add_xform_to_header -c /usr/local/freesurfer/subjects/3220/mri/transforms/talairach.xfm /usr/local/freesurfer/subjects/3220/mri/orig.mgz /usr/local/freesurfer/subjects/3220/mri/orig.mgz 
+
+#--------------------------------------------
+#@# Talairach Mon Jan  1 19:39:36 UTC 2018
+
+ mri_nu_correct.mni --no-rescale --i orig.mgz --o orig_nu.mgz --n 1 --proto-iters 1000 --distance 50 
+
+
+ talairach_avi --i orig_nu.mgz --xfm transforms/talairach.auto.xfm 
+
+talairach_avi log file is transforms/talairach_avi.log...
+
+ cp transforms/talairach.auto.xfm transforms/talairach.xfm 
+
+#--------------------------------------------
+#@# Talairach Failure Detection Mon Jan  1 19:44:30 UTC 2018
+
+ talairach_afd -T 0.005 -xfm transforms/talairach.xfm 
+
+
+ awk -f /usr/local/freesurfer/bin/extract_talairach_avi_QA.awk /usr/local/freesurfer/subjects/3220/mri/transforms/talairach_avi.log 
+
+
+ tal_QC_AZS /usr/local/freesurfer/subjects/3220/mri/transforms/talairach_avi.log 
+
+#--------------------------------------------
+#@# Nu Intensity Correction Mon Jan  1 19:44:30 UTC 2018
+
+ mri_nu_correct.mni --i orig.mgz --o nu.mgz --uchar transforms/talairach.xfm --n 2 
+
+
+ mri_add_xform_to_header -c /usr/local/freesurfer/subjects/3220/mri/transforms/talairach.xfm nu.mgz nu.mgz 
+
+#--------------------------------------------
+#@# Intensity Normalization Mon Jan  1 19:53:45 UTC 2018
+
+ mri_normalize -g 1 -mprage nu.mgz T1.mgz 
+
+#--------------------------------------------
+#@# Skull Stripping Mon Jan  1 19:58:27 UTC 2018
+
+ mri_em_register -rusage /usr/local/freesurfer/subjects/3220/touch/rusage.mri_em_register.skull.dat -skull nu.mgz /usr/local/freesurfer/average/RB_all_withskull_2016-05-10.vc700.gca transforms/talairach_with_skull.lta 
+
+
+ mri_watershed -rusage /usr/local/freesurfer/subjects/3220/touch/rusage.mri_watershed.dat -T1 -brain_atlas /usr/local/freesurfer/average/RB_all_withskull_2016-05-10.vc700.gca transforms/talairach_with_skull.lta T1.mgz brainmask.auto.mgz 
+
+
+ cp brainmask.auto.mgz brainmask.mgz 
+
+#-------------------------------------
+#@# EM Registration Mon Jan  1 21:12:17 UTC 2018
+
+ mri_em_register -rusage /usr/local/freesurfer/subjects/3220/touch/rusage.mri_em_register.dat -uns 3 -mask brainmask.mgz nu.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta 
+
+#--------------------------------------
+#@# CA Normalize Mon Jan  1 22:09:15 UTC 2018
+
+ mri_ca_normalize -c ctrl_pts.mgz -mask brainmask.mgz nu.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta norm.mgz 
+
+#--------------------------------------
+#@# CA Reg Mon Jan  1 22:14:06 UTC 2018
+
+ mri_ca_register -rusage /usr/local/freesurfer/subjects/3220/touch/rusage.mri_ca_register.dat -nobigventricles -T transforms/talairach.lta -align-after -mask brainmask.mgz norm.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.m3z 
+
---
+++ 3224/scripts/recon-all.cmd
@@ -0,0 +1,80 @@
+
+
+#---------------------------------
+# New invocation of recon-all Mon Jan  1 19:40:18 UTC 2018 
+
+ mri_convert /home/ubuntu/sMRI/3224.nii /usr/local/freesurfer/subjects/3224/mri/orig/001.mgz 
+
+#--------------------------------------------
+#@# MotionCor Mon Jan  1 19:40:23 UTC 2018
+
+ cp /usr/local/freesurfer/subjects/3224/mri/orig/001.mgz /usr/local/freesurfer/subjects/3224/mri/rawavg.mgz 
+
+
+ mri_convert /usr/local/freesurfer/subjects/3224/mri/rawavg.mgz /usr/local/freesurfer/subjects/3224/mri/orig.mgz --conform 
+
+
+ mri_add_xform_to_header -c /usr/local/freesurfer/subjects/3224/mri/transforms/talairach.xfm /usr/local/freesurfer/subjects/3224/mri/orig.mgz /usr/local/freesurfer/subjects/3224/mri/orig.mgz 
+
+#--------------------------------------------
+#@# Talairach Mon Jan  1 19:40:43 UTC 2018
+
+ mri_nu_correct.mni --no-rescale --i orig.mgz --o orig_nu.mgz --n 1 --proto-iters 1000 --distance 50 
+
+
+ talairach_avi --i orig_nu.mgz --xfm transforms/talairach.auto.xfm 
+
+talairach_avi log file is transforms/talairach_avi.log...
+
+ cp transforms/talairach.auto.xfm transforms/talairach.xfm 
+
+#--------------------------------------------
+#@# Talairach Failure Detection Mon Jan  1 19:46:32 UTC 2018
+
+ talairach_afd -T 0.005 -xfm transforms/talairach.xfm 
+
+
+ awk -f /usr/local/freesurfer/bin/extract_talairach_avi_QA.awk /usr/local/freesurfer/subjects/3224/mri/transforms/talairach_avi.log 
+
+
+ tal_QC_AZS /usr/local/freesurfer/subjects/3224/mri/transforms/talairach_avi.log 
+
+#--------------------------------------------
+#@# Nu Intensity Correction Mon Jan  1 19:46:32 UTC 2018
+
+ mri_nu_correct.mni --i orig.mgz --o nu.mgz --uchar transforms/talairach.xfm --n 2 
+
+
+ mri_add_xform_to_header -c /usr/local/freesurfer/subjects/3224/mri/transforms/talairach.xfm nu.mgz nu.mgz 
+
+#--------------------------------------------
+#@# Intensity Normalization Mon Jan  1 20:00:49 UTC 2018
+
+ mri_normalize -g 1 -mprage nu.mgz T1.mgz 
+
+#--------------------------------------------
+#@# Skull Stripping Mon Jan  1 20:07:49 UTC 2018
+
+ mri_em_register -rusage /usr/local/freesurfer/subjects/3224/touch/rusage.mri_em_register.skull.dat -skull nu.mgz /usr/local/freesurfer/average/RB_all_withskull_2016-05-10.vc700.gca transforms/talairach_with_skull.lta 
+
+
+ mri_watershed -rusage /usr/local/freesurfer/subjects/3224/touch/rusage.mri_watershed.dat -T1 -brain_atlas /usr/local/freesurfer/average/RB_all_withskull_2016-05-10.vc700.gca transforms/talairach_with_skull.lta T1.mgz brainmask.auto.mgz 
+
+
+ cp brainmask.auto.mgz brainmask.mgz 
+
+#-------------------------------------
+#@# EM Registration Mon Jan  1 21:27:42 UTC 2018
+
+ mri_em_register -rusage /usr/local/freesurfer/subjects/3224/touch/rusage.mri_em_register.dat -uns 3 -mask brainmask.mgz nu.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta 
+
+#--------------------------------------
+#@# CA Normalize Mon Jan  1 22:17:53 UTC 2018
+
+ mri_ca_normalize -c ctrl_pts.mgz -mask brainmask.mgz nu.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.lta norm.mgz 
+
+#--------------------------------------
+#@# CA Reg Mon Jan  1 22:22:11 UTC 2018
+
+ mri_ca_register -rusage /usr/local/freesurfer/subjects/3224/touch/rusage.mri_ca_register.dat -nobigventricles -T transforms/talairach.lta -align-after -mask brainmask.mgz norm.mgz /usr/local/freesurfer/average/RB_all_2016-05-10.vc700.gca transforms/talairach.m3z 
+
